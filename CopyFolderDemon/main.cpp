b7c2618758da67709a77ed82ec9708f9f84bc8fa
noxydee <37812367+noxydee@users.noreply.github.com>
2021-04-27 12:30:46 +0200

    Commit v1
    Dodano: x

--- CopyFolderDemon/main.cpp
+++ CopyFolderDemon/main.cpp
@@ -12,6 +12,17 @@
 #include <signal.h>
 #include <unistd.h>
 
+typedef struct ListOfElements{
+    struct dirent** SourceElements;
+    struct dirent** TargetElements;
+    struct dirent** SourceDirectories;
+    struct dirent** TargetDirectories;
+    int NumberOfSourceElements;
+    int NumberOfTargetElements;
+    int NumberOfSourceDirectories;
+    int NumberOfTargetDirectories;
+} ListOfElements;
+
 
 int Copy_y(char *file0,char *file1,int size) {
     int input, output;
@@ -45,7 +56,7 @@ char* MergeStrings(char * first,char * second)
     asprintf(&CreatedString,"%s%s",first,second);
     return CreatedString;
 }
-bool FileIsFile(char *file)
+bool FileIsFile(const char *file)
 {
     struct stat FileStat;
     if(stat(file,&FileStat)== -1)
@@ -60,7 +71,7 @@ bool FileIsFile(char *file)
         return false;
     }
 }
-bool FileIsDirectory(char *file)
+bool FileIsDirectory(const char *file)
 {
     struct stat FileStat;
     if(stat(file,&FileStat)==-1){
@@ -76,28 +87,20 @@ bool FileIsDirectory(char *file)
 }
 char* AddSlashToPath(char *source)
 {
-    if(source[strlen(source)] != '/'){
-        return MergeStrings(source,"/");
+    if(source[strlen(source)-1] != '/'){
+        source= MergeStrings(source,"/");
+        source[strlen(source)]='\0';
     }
-    else{
-        return source;
-    }
+    return source;
 }
-int MergeDirectories(char *source,char* target)
-{
-    //struct listed_entries entries;
-
-}
-int GetOnlyFiles(struct dirent* Element)
+int GetOnlyFiles(const struct dirent* Element)
 {
     return FileIsFile(Element->d_name);
 }
-int GetOnlyDirecotires(struct dirent* Element)
+int GetOnlyDirectoires(const struct dirent* Element)
 {
     return FileIsDirectory(Element->d_name);
 }
-
-
 
 
 int SearchForChanges(char *path)
@@ -112,7 +115,7 @@ void InitDeamon(char *source,char *target) {
 
 
 }
-int GetListOfDirectories(char *source,char *target,struct ListOfElements* List)
+int GetListOfDirectories(char *source,char *target,struct ListOfElements* Elements)
 {
     DIR* SourceDirectory;
     DIR* TargetDirectory;
@@ -123,12 +126,58 @@ int GetListOfDirectories(char *source,char *target,struct ListOfElements* List)
     struct dirent** TargetDirectoriesList;
     int NumberOfElements_Source =0;
     int NumberOfElements_Target=0;
+    int NumberOfDirectories_Source=0;
+    int NumberOfDirectories_Target=0;
 
     SourceDirectory = opendir(source);
     TargetDirectory = opendir(target);
 
     chdir(source);
-    NumberOfElements_Source = scandir(source,&SourceFilesList,GetOnlyFiles,alphasort);
+    NumberOfElements_Source = scandir(source,&SourceFilesList,GetOnlyFiles, alphasort);
+    NumberOfDirectories_Source = scandir(source,&SourceDirectoriesList,GetOnlyDirectoires, alphasort);
+    chdir(target);
+    NumberOfElements_Source = scandir(target,&TargetFilesList,GetOnlyFiles, alphasort);
+    NumberOfDirectories_Target = scandir(target,&TargetDirectoriesList,GetOnlyDirectoires, alphasort);
+
+    if(NumberOfElements_Source<0){
+        return 0;
+    }
+    else if(NumberOfElements_Target<0){
+        return 0;
+    }
+
+    //Pliki
+    Elements->NumberOfSourceElements=NumberOfElements_Source;
+    Elements->NumberOfTargetElements=NumberOfElements_Target;
+    Elements->SourceElements=SourceFilesList;
+    Elements->TargetElements=TargetFilesList;
+    //Katalogi
+    Elements->NumberOfSourceDirectories = NumberOfDirectories_Source;
+    Elements->NumberOfTargetDirectories = NumberOfDirectories_Target;
+    Elements->SourceDirectories = SourceDirectoriesList;
+    Elements->TargetDirectories = TargetDirectoriesList;
+
+    return 1;
+}
+int MergeDirectories(char *source,char* target)
+{
+    struct ListOfElements Elements;
+    int found=0;
+    GetListOfDirectories(source,target,&Elements);
+    chdir(target);
+
+    for(int i=0;i<Elements.NumberOfSourceDirectories;i++){
+        for(int ii=0;ii<Elements.NumberOfTargetDirectories;ii++) {
+
+            if(strcmp(Elements.SourceDirectories[i]->d_name,Elements.TargetDirectories[ii]->d_name) ==0)
+            {
+                found=0;
+            }
+
+        }
+    }
+
+
 
 }
 
@@ -137,7 +186,7 @@ int main(/*int argc,char *argv[]*/)
 	//char *source = argv[1];
 	//char *target = argv[2];
 
-	char *source = "TestObjects/SourceFolder/plik0.txt";
+	char *source = "TestObjects/SourceFolder";
 	char *target ="TestObjects/DestinationFolder";
 
 
